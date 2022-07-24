1fe61bac8b413357894c5b928447d99621e807fc
Tram Minh Nhut <tmnhut1701@gmail.com>
2022-07-24 21:42:02 +0700

    feat : controller and views

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -3,7 +3,7 @@ import express from "express";
 let configViewEngine = (app) => {
     app.use(express.static(__dirname + 'public'));
     app.set("view engine", "ejs");
-    app.set("view", "../views")
+    app.set("views", "./src/views")
 }
 
 module.exports = configViewEngine
--- src/router/web.js
+++ src/router/web.js
@@ -1,11 +1,19 @@
 import express from "express";
+import homtroller from "../controllers/homeController"
 
 let router = express.Router();
 
 let initWebRouters = (app) => {
-    router.get('/', (req , res) => {
+    router.get('/', homtroller.getHomePage)
+
+    router.get('/test', (req , res) => {
         return res.send("hello day la bai dau tien cua toi!!!");
     })
+
+
+    
+
+
     return app.use("/", router)
 
 }
